d73c78be62c42e6fee8b7dfd093d7ba6db08dc43
David Nilsson <davidn5013@yahoo.se>
2023-01-09 13:21:18 +0100

    delete loop.cmd

--- loop.cmd
+++
@@ -1,7 +0,0 @@
-:: This only work on my machine. 
-:: bg is my script for running in bakground like & in bash
-:: watchrun can be downloaded, 
-:: clear is part of githubs suit
-:: gchk just my script for running  2 extra linter, one error check and code examinator
-@watchrun -care *.go -interval 1700ms bg "clear & go vet ./... && go test ./... && gchk ./..."
-
