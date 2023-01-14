5a23e96859d090dc04145b1b1ac584a8645fed63
David Nilsson <davidn5013@yahoo.se>
2023-01-14 20:36:00 +0100

    Grind 35 implementing trie finished

---
+++ tools/ds/35_implement_trie.go
@@ -0,0 +1,57 @@
+package ds
+
+// implementation of trie
+// Leetcode 208
+// Grind75 number 35
+
+// Trie node store word for easy find
+type Trie struct {
+	words []string
+}
+
+// Constructor same as NewTrie to comply with leetcode
+func Constructor() Trie {
+	return NewTrie()
+}
+
+// NewTrie pointer to new trie struct
+func NewTrie() Trie {
+	return Trie{
+		words: []string{},
+	}
+}
+
+// normal idiomatic go is (t *Trie) but this for compliance to leetcode specs
+
+// Insert new order
+func (t *Trie) Insert(word string) {
+	t.words = append(t.words, word)
+}
+
+// Search for word in trie
+func (t *Trie) Search(word string) bool {
+	for _, data := range t.words {
+		if data == word {
+			return true
+		}
+	}
+
+	return false
+}
+
+// StartsWith for word starting with in trie
+func (t *Trie) StartsWith(prefix string) bool {
+	lenPrefix := len(prefix)
+
+	for _, word := range t.words {
+		if len(word) < lenPrefix {
+			continue
+		}
+
+		if prefix == word[:lenPrefix] {
+			return true
+		}
+	}
+
+	return false
+}
