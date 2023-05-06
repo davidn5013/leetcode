936e7e0a24d1b917d00da34fd317125855810fac
David Nilsson <davidn5013@yahoo.se>
2023-05-06 21:04:38 +0200

    optimze try on ransome

--- grind75/15ransomnote.go
+++ grind75/15ransomnote.go
@@ -8,14 +8,60 @@ import (
 // a taken from b false
 // aa taken from ab false
 // aa taken from aab true
-func CanConstruct(a string, b string) bool {
-	strip := strings.ToLower(b)
-	for _, r := range strings.ToLower(a) {
-		prev := strip
-		strip = strings.Replace(strip, string(r), "", 1)
-		if strings.Compare(prev, strip) == 0 {
+func canConstruct(ransom string, magazine string) bool {
+	magBytearr := []byte(strings.ToLower(magazine))
+	for _, r := range []byte(strings.ToLower(ransom)) {
+		found := false
+		for i, s := range magBytearr {
+			if r >= 'a' && r <= 'z' && r == s {
+				found = true
+				magBytearr[i] = 0
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+
+	}
+	return true
+}
+
+func canConstruct2(ransom string, magazine string) bool {
+	magaLcase := strings.ToLower(magazine)
+	for _, r := range strings.ToLower(ransom) {
+		found := false
+		if strings.Contains(magaLcase, string(r)) {
+			found = true
+			magaLcase = strings.Replace(magaLcase, string(r), "", 1)
+			break
+		}
+		if !found {
 			return false
 		}
 	}
+	return true
+}
+
+func canConstruct3(ransom string, magazine string) bool {
+	ransom = strings.ToUpper(ransom)
+	magazine = strings.ToUpper(magazine)
+	letterCount := [25]int{}
+
+	for _, v := range magazine {
+		if v >= 'A' && v <= 'Z' {
+			letterCount[v-'A']++
+		}
+	}
+
+	for _, v := range ransom {
+		if v >= 'A' && v <= 'Z' {
+			if letterCount[v-'A'] == 0 {
+				return false
+			}
+			letterCount[v-'A']--
+		}
+	}
+
 	return true
 }
--- grind75/15ransomnote_test.go
+++ grind75/15ransomnote_test.go
@@ -1,10 +1,53 @@
 package grind75
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-func ExampleCanConstruct() {
+func Example_canConstruct() {
+	fmt.Printf("%#v ", canConstruct("Hello", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct("HELLO", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct("abcdef", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct("Hello", "tere lolo"))
+	fmt.Printf("%#v ", canConstruct2("Hello", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct2("HELLO", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct2("abcdef", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct2("Hello", "tere lolo"))
+	fmt.Printf("%#v ", canConstruct3("Hello", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct3("HELLO", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct3("abcdef", "Hi there lolo"))
+	fmt.Printf("%#v ", canConstruct3("Hello", "tere lolo"))
+	// OutPut: true true false false true true false false true true false false
+}
+
+func Example_canConstruct2() {
 	ransom := "Hello"
 	magazine := "Hi there lolo"
-	fmt.Printf("Value: %#v\n", CanConstruct(ransom, magazine))
+	fmt.Printf("Value: %#v\n", canConstruct2(ransom, magazine))
 	// OutPut: Value: true
+}
+
+func Benchmark_canConstruct(b *testing.B) {
+	b.Run("using byte array", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ransom := "Hello"
+			magazine := "Hi there lolo"
+			canConstruct(ransom, magazine)
+		}
+	})
+	b.Run("using strings", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ransom := "Hello"
+			magazine := "Hi there lolo"
+			canConstruct2(ransom, magazine)
+		}
+	})
+	b.Run("using strings", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ransom := "Hello"
+			magazine := "Hi there lolo"
+			canConstruct3(ransom, magazine)
+		}
+	})
 }
